38d59003be1a69e6be72e00ddd20be76f0e87543
Laëtitia Delcoigne <l_delcoigne@orsysformation.fr>
2021-10-27 10:30:31 +0100

    1

---
+++ monappli-web/src/main/java/servlets/MaServlet.java
@@ -0,0 +1,61 @@
+
+package servlets;
+
+
+
+/**
+
+ * Servlet implementation class MaServlet
+
+ */
+
+public class MaServlet {
+
+    private static final long serialVersionUID = 1L;
+
+
+
+    /**
+
+     * Default constructor.
+
+     */
+
+    public MaServlet() {
+
+        // TODO Auto-generated constructor stub
+
+    }
+
+
+
+    /**
+
+     * Returns the reponse
+
+     */
+
+    protected void doGet(String request, String response)  {
+
+       //nothing to do
+
+    }
+
+
+
+    /**
+
+     * Post the message
+
+     */
+
+    protected void doPost( String request, String response) {
+
+
+        doGet(request, response);
+
+    }
+
+
+
+}
